3f963fb1d05ce17b9ed29a07b50616de51cd4708
andrewbingham1973 <andrewbingham@gmail.com>
2019-01-20 19:31:47 +0000

    Update core.js
    updated to get

--- core.js
+++ core.js
@@ -25,51 +25,21 @@ function changeAppPage(gameScreen) {
 
 
 
-/*
-//Get content
-function getPartialView(screen) {
-  var contentLoaded ;
-  console.log("screen content injection for "+screen);
-  $.get('partialViews/'+screen+'.html', function(data) {
-    //inject the content into the DOM
-    $(".contentRoot").append(data);
-    contentLoaded = true;
+// Get content
+// updated for myquest 5/1/19
+function getPartialView (screen) {
 
-  }); //end get
-
-
-} //close getPartialView function
-*/
-
-
-function getPartialView(screen) {
-
-
-
-  var contentLoaded;
-
-  console.log("screen content injection for " + screen);
-
-
-  //Get HTML via a promise
-  var myHeaders = new Headers();
-  var myInit = {
-    method: 'GET',
-    headers: myHeaders,
-    mode: 'cors',
-    cache: 'default'
-  };
-  var myRequest = new Request('partialViews/' + screen + '.html', myInit);
-  fetch(myRequest).then(function(response) {
-    return response.text();
-  }).then(function(data) {
-    $(".contentRoot").append(data);
-    contentLoaded = true;
-
-  });
+  var contentLoaded
+  //console.log('screen content injection for ' + screen)
+  $.get('partialViews/' + screen + '.html', function (data) {
+    // inject the content into the DOM
+   $(".contentRoot").append(data);
+    contentLoaded = true
+    
+  }) // end get
 
 
 
 
+} // close getPartialView function
 
-} //close getPartialView function
